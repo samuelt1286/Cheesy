45d5727e6c1ce1a85e3f00bc94ecb41d7db8d290
samuelt1286 <samuelt@vt.edu>
2022-11-28 19:06:31 -0500

    Update Cheese.java

--- Cheese.java
+++ Cheese.java
@@ -4,40 +4,72 @@ import java.util.ArrayList;
  * Creating a class to document and organize cheese according to quality
  *
  * @author Samuelt
- * @version Oct 20, 2022
+ * @version Nov 20, 2022
  *
  */
 public class Cheese {
     private int qualityCheck;
+    public int bounds = qualityCheck;
     private String name;
     ArrayList<Integer> cheeseArray = new ArrayList<Integer>();
     
     
+    /**
+     * Construct a new Cheese object.
+     *
+     * @param cheeseName - name of cheese
+     * @param grade - expected quality of cheese
+     */
     public Cheese(String cheeseName, int grade) {
         qualityCheck = grade;  
         name = cheeseName;
     }
     
+    /**
+     * This function returns the quality of the cheese/object.
+     *
+     * @return this.qualityCheck
+     */
     public int getCheeseQuality() {
         return this.qualityCheck;
     }
     
+    /**
+     * This function shows the expected quality of the cheese.
+     *
+     */
     public void printCheeseQuality() {
         System.out.println(name + " expected quality -> " + qualityCheck);
     }
     
+    /**
+     * This function returns the name of the cheese/object.
+     *
+     * @return name
+     */
     public String getName() {
         System.out.print(name);
         return name;
     }
     
     
+    /**
+     * This function checks the quality of the cheese and returns false if not to standard.
+     *
+     * @param cheeseinQuestion - quality of current cheese/object
+     * @return - whether good or not
+     */
     public boolean qualityCheck(int cheeseinQuestion) {
         if (cheeseinQuestion != qualityCheck) {  //better way of doing this
             return true;
         }
         return false;
     }
+    /**
+     * This function checks the quality of the cheeses in the system and deletes
+     * what is not to standard.
+     *
+     */
     public void qualityManagement() {
         //sort through list and delete
         int returnCheck = cheeseArray.size();
@@ -52,24 +84,44 @@ public class Cheese {
         }
     }
     
-    public int returnCheeseArray() {
-        getName();
+    /**
+     * This function returns the current amount of cheese with Recursion
+     *
+     * @param arraySize the amount of cheese of that type
+     */
+    public void returnCheeseArray(int arraySize) {
         System.out.print("{");
-        for (int i = 0; i < cheeseArray.size(); i++) {
-            System.out.print(cheeseArray.get(i));
+        getName();
+        if ((arraySize) < 0) {
+            return;
         }
-        System.out.println("}");
-        return 0;
+        System.out.println(" " + cheeseArray.get(arraySize) + "}");
+        returnCheeseArray(arraySize - 1);
     }
     
+    
+    /**
+     * This function sets the quality of the cheese if needed to change
+     *
+     * @param setQuality - quality of cheese
+     */
     public void setCheese(int setQuality) {
         this.qualityCheck = setQuality;
     }
     
+    /**
+     * This function adds a new cheese/object to the type of cheese
+     *
+     * @param cheeseQuality - current quality of new cheese
+     */
     public void addCheese(int cheeseQuality) {
         cheeseArray.add(cheeseQuality);
     }
     
+    /**
+     * Main method, executing the class 
+     *
+     */
     public static void main(String []args) {
         Cheese Cheddar = new Cheese("Cheddar", 5);
         Cheese Swiss = new Cheese("Swiss",6);
@@ -83,17 +135,21 @@ public class Cheese {
         Cheddar.addCheese(4);
         Cheddar.addCheese(5);
         Cheddar.addCheese(5);
-        Cheddar.returnCheeseArray();
+        Cheddar.returnCheeseArray(Cheddar.cheeseArray.size()-1);
         Cheddar.qualityManagement();
-        Cheddar.returnCheeseArray();
+        Cheddar.returnCheeseArray(Cheddar.cheeseArray.size()-1);
         Swiss.addCheese(4);
         Swiss.addCheese(6);
         Swiss.addCheese(6);
         Swiss.addCheese(8);
-        Swiss.returnCheeseArray();
+        Swiss.returnCheeseArray(Swiss.cheeseArray.size()-1);
         Swiss.qualityManagement();
-        Swiss.returnCheeseArray();
+        Swiss.returnCheeseArray(Swiss.cheeseArray.size()-1);
         Gouda.addCheese(8);
-        Gouda.returnCheeseArray();  
-    } 
+        Gouda.returnCheeseArray(Gouda.cheeseArray.size()-1);
+        
+        
+    }
+
+  
 }
